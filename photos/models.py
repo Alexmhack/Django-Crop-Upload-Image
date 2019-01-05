56305488f2c541ae87ad8ad4bf1c28a93f960623
alexHooper <alexmohack@gmail.com>
2019-01-05 18:57:51 +0530

    create photos model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Photo(models.Model):
+	title = models.CharField(max_length=144)
+	image_heigth = models.IntegerField()
+	image_width = models.IntegerField()
+	image = models.ImageField(
+		upload_to='media/%Y/%m/%d/',
+		height_field=image_heigth,
+		width_field=image_width
+	)
+	timestamp = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return str(self.title)
