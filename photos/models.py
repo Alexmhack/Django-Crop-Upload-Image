c28bb99584eac30e372ab4d3b92763b667f89f94
alexHooper <alexmohack@gmail.com>
2019-01-05 20:36:58 +0530

    make field attrs str

--- photos/models.py
+++ photos/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 class Photo(models.Model):
 	title = models.CharField(max_length=144)
+	image = models.ImageField(
+		upload_to='%Y/%m/%d/',
+		height_field='image_heigth',
+		width_field='image_width'
+	)
 	image_heigth = models.IntegerField()
 	image_width = models.IntegerField()
-	image = models.ImageField(
-		upload_to='media/%Y/%m/%d/',
-		height_field=image_heigth,
-		width_field=image_width
-	)
 	timestamp = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
 
