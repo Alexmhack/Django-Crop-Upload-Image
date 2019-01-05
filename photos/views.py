50983ff17ce430cea85167baae29b9b50ceda877
alexHooper <alexmohack@gmail.com>
2019-01-05 20:39:03 +0530

    create photo form list view

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib import messages
 
-# Create your views here.
+from .models import Photo
+from .forms import PhotoForm
+
+class PhotoView(View):
+	def get(self, request):
+		photos = Photo.objects.all()
+		form = PhotoForm
+		context = {
+			'photos': photos,
+			'form': form
+		}
+		return render(request, 'photos/photo_form.html', context)
+
+	def post(self, request):
+		form = PhotoForm(request.POST, request.FILES)
+		if form.is_valid():
+			form.save()
+			# display a success message
+			return redirect('photos')
