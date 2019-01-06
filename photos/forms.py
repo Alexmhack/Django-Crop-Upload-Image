4f77e4bc21446ecf382ad5165aa07d3e9b178252
alexHooper <alexmohack@gmail.com>
2019-01-06 09:12:57 +0530

    correct seek file error

--- photos/forms.py
+++ photos/forms.py
@@ -5,13 +5,26 @@ from django.core.files import File
 from .models import Photo
 
 class PhotoForm(forms.ModelForm):
+	x = forms.FloatField(widget=forms.HiddenInput())
+	y = forms.FloatField(widget=forms.HiddenInput())
+	width = forms.FloatField(widget=forms.HiddenInput())
+	height = forms.FloatField(widget=forms.HiddenInput())
+
 	class Meta:
 		model = Photo
-		fields = ('title', 'image')
+		fields = ('title', 'image', 'x', 'y', 'height', 'width')
 
 	def save(self):
-		photo = super().save()
-		width = self.cleaned_data.get('image_width')
-		heigth = self.cleaned_data.get('image_heigth')
-		print(width, heigth)
-		return photo
+		photo = super(PhotoForm, self).save(commit=False)
+
+		x = self.cleaned_data.get('x')
+		y = self.cleaned_data.get('y')
+		w = self.cleaned_data.get('width')
+		h = self.cleaned_data.get('height')
+
+		image = Image.open(photo.image)
+		cropped_image = image.crop((x, y, w + x, h + y))
+		resized_image = cropped_image.resize((200, 200), Image.ANTIALIAS)
+		resized_image.save(photo.image.path)
+
+		return super(PhotoForm, self).save(commit=True)
