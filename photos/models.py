a9c31cf909eaf2f73848af2cf6b4f813600b5404
alexHooper <alexmohack@gmail.com>
2019-01-06 12:18:00 +0530

    change hieght model field

--- photos/models.py
+++ photos/models.py
@@ -5,10 +5,10 @@ class Photo(models.Model):
 	title = models.CharField(max_length=144)
 	image = models.ImageField(
 		upload_to='%Y/%m/%d/',
-		height_field='image_heigth',
+		height_field='image_height',
 		width_field='image_width'
 	)
-	image_heigth = models.FloatField()
+	image_height = models.FloatField()
 	image_width = models.FloatField()
 	timestamp = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
