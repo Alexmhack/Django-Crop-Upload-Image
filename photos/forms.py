1e553740ed80f3da8972a95fdd4470ac42cb9fdb
alexHooper <alexmohack@gmail.com>
2019-01-06 12:17:29 +0530

    change model form fields

--- photos/forms.py
+++ photos/forms.py
@@ -8,11 +8,11 @@ class PhotoForm(forms.ModelForm):
 	x = forms.FloatField(widget=forms.HiddenInput())
 	y = forms.FloatField(widget=forms.HiddenInput())
 	image_width = forms.FloatField(widget=forms.HiddenInput())
-	image_heigth = forms.FloatField(widget=forms.HiddenInput())
+	image_height = forms.FloatField(widget=forms.HiddenInput())
 
 	class Meta:
 		model = Photo
-		fields = ('title', 'image', 'x', 'y', 'image_heigth', 'image_width')
+		fields = ('title', 'image', 'x', 'y', 'image_height', 'image_width')
 
 	def save(self):
 		photo = super(PhotoForm, self).save(commit=False)
@@ -20,7 +20,7 @@ class PhotoForm(forms.ModelForm):
 		x = self.cleaned_data.get('x')
 		y = self.cleaned_data.get('y')
 		w = self.cleaned_data.get('image_width')
-		h = self.cleaned_data.get('image_width')
+		h = self.cleaned_data.get('image_height')
 
 		image = Image.open(photo.image)
 		cropped_image = image.crop((x, y, w + x, h + y))
